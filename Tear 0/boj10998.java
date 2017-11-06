84c771259ce14d501a2007c518430066dda35c04
munak <oiool@naver.com>
2017-11-07 02:19:59 +0900

    10998. A*B

--- Tear 0/boj10998.java
+++ Tear 0/boj10998.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main{
 	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
+		
         int result = 1;
 
         result = result * sc.nextInt() * sc.nextInt();
