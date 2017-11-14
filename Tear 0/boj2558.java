900e4da15076ac7062b1ebd4bcd4071eb55af73c
munak <oiool@naver.com>
2017-11-14 15:25:53 +0900

    2558. A+B - 2

---
+++ Tear 0/boj2558.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		
+		for(int i = 0; i < 2; i++) {
+			sum += sc.nextInt();
+		}
+		sc.close();
+		System.out.println(sum);
+	}
+}
