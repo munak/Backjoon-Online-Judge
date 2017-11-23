8242a32123fc88146b32380b584e6e3cdd110ba8
munak <oiool@naver.com>
2017-11-24 04:27:02 +0900

    2439. 별찍기 - 2

---
+++ Tear 0/boj2439.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int in = sc.nextInt();
+
+		for(int i = 1; i <= in; i++) {
+			for(int j = 0; j < in - i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
