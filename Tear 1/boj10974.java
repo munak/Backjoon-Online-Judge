5441e70a6a1eeeea675280213109ea227bed8254
munak <oiool@naver.com>
2017-12-02 18:58:57 +0900

    10974. 모든 순열

---
+++ Tear 1/boj10974.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	static int count;
+	static List<String> resultNum;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		count = sc.nextInt();
+		sc.close();
+		
+		int[] num = new int[count];
+		resultNum = new ArrayList<String>();
+		
+		for(int i = 0; i < count; i++) {
+			num[i] = i + 1;
+		}
+		
+ 		dfs(num,0);
+		
+ 		Collections.sort(resultNum);
+ 		
+ 		for(String view : resultNum) {
+ 			System.out.println(view);
+ 		}
+	}
+	
+	static void dfs(int[] arr, int start) {
+		if(start == count) {
+			storing(arr);
+			return ;
+		}
+		
+		for(int i = start; i < count; i++) {
+			trans(arr,start,i);
+			dfs(arr,start + 1);
+			trans(arr,start,i);
+		}
+	}
+	
+	static void trans(int[] arr, int s, int e) {
+		int temp = arr[s];
+		arr[s] = arr[e];
+		arr[e] = temp;
+	}
+	
+	static void storing(int[] arr) {
+		String reNum = "";
+		for(int i = 0; i < count; i++) {
+			reNum += arr[i] + " ";
+		}
+		resultNum.add(reNum);
+	}
+}
