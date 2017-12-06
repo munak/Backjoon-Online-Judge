04cc0b7b183b81a5778ea821f335d00bc5fe08b5
munak <oiool@naver.com>
2017-12-06 22:23:32 +0900

    11654. 아스키 코드

---
+++ Tear 0/boj11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		char in = sc.next().charAt(0);
+		
+		System.out.println((int)in);
+	}
+}
