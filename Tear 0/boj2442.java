d7419ae76f981c5fda4ba5771e2dfd786e0a46b6
munak <oiool@naver.com>
2018-01-23 22:58:58 +0900

    2442. 별찍기 - 5

---
+++ Tear 0/boj2442.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i = 0; i < count; i++) {
+			for(int j = 0; j < count - 1 - i; j++) {
+				System.out.print(" ");
+			}
+			
+			for(int j = 0; j < 1 + i * 2; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
