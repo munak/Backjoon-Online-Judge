572c047d61798079407f20a3fe226a0b020a2116
munak <oiool@naver.com>
2018-03-26 01:14:21 +0900

    3046. R2

---
+++ Tear 0/boj3046.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		br.close();
+		
+		bw.write((Integer.parseInt(st.nextToken()) - 2 * Integer.parseInt(st.nextToken())) * -1 + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
